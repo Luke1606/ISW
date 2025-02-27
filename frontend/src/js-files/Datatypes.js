ad54c74a445b38af95ec0cd37d6d29677d1516a4
Luke1606 <luisalbertohedzro@gmail.com>
2025-02-27 07:26:25 -0500

    Se agregaron los archivos js en una carpeta separada para simplificar ciertos procesos

---
+++ frontend/src/js-files/Datatypes.js
@@ -0,0 +1,12 @@
+const datatypes = {
+    user: 'user',
+    student: 'student',
+    professor: 'professor',
+    evidence: 'evidence',
+    request: 'request',
+    defense_tribunal: 'defense_tribunal',
+    tribunal: 'tribunal',
+    defense_act: 'defense_act'
+}
+
+export default datatypes
