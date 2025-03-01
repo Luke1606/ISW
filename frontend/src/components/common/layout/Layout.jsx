abb9252e666df31e8d68d3fd18e6868cd31f789c
Luke1606 <luisalbertohedzro@gmail.com>
2025-03-01 11:13:28 -0500

    Se removio el style y se llevo al index.css y se le agrego un className al outlet

--- frontend/src/components/common/layout/Layout.jsx
+++ frontend/src/components/common/layout/Layout.jsx
@@ -1,20 +1,18 @@
 import { Outlet } from "react-router-dom"
-import bg from "../../../assets/background.jpg"
 import Header from "./Header"
 import Footer from "./Footer"
 
 const Layout = () => {
     return (
             <div 
-                className="layout-container" 
-                style={{background: `linear-gradient(to right, rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.3)), url(${bg})` }}>
+                className="layout-container">
                 <Header />
                     <main>
-                            <Outlet />
+                        <Outlet className="main-content"/>
                     </main> 
                 <Footer />
             </div>
-            )
+        )
 }
 
 export default Layout
