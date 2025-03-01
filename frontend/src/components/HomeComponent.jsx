8ff118245236e318b67c55e66734ac56f11c04a2
Luke1606 <luisalbertohedzro@gmail.com>
2025-03-01 11:10:00 -0500

    Se renombro el componente y se le dio una estructura basica para depuracion

--- frontend/src/components/HomeComponent.jsx
+++ frontend/src/components/HomeComponent.jsx
@@ -1,14 +1,18 @@
-const Home = () => {
+import { Link } from "react-router-dom"
+const HomeComponent = () => {
     return (
-        <div>
+        <>
             <h1>
-                pico
+                home
             </h1>
             <p>
                 Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorem, expedita cupiditate doloribus tenetur ab quo minima blanditiis soluta doloremque cumque provident tempore ex, rerum distinctio itaque corporis, reprehenderit eos totam!
             </p>
-        </div>
+            <button>
+                <Link to="/login">Acceder</Link>
+            </button>
+        </>
     )
 }
 
-export default Home
+export default HomeComponent
