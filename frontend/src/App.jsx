169983516c3c91ac5a955fce778f151ac3676e4f
Luke1606 <luisalbertohedzro@gmail.com>
2025-03-01 11:14:25 -0500

    Se cambio la estructura de rutas para que el login tambien tenga el layout, proximamente se adaptara la estructura del header a este componente

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,9 @@
 import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from "react-router-dom"
-import UserComponent from "./components/login/UserComponent"
-import { UserProvider } from "./contexts/UserContext"
-import Home from "./components/Home"
-import ProtectedRoutesComponent from "./components/ProtectedRoutesComponent"
 import Layout from "./components/common/layout/Layout"
+import HomeComponent from "./components/HomeComponent"
+import LoginComponent from "./components/login/LoginComponent"
+import { UserProvider } from "./contexts/UserContext"
+import ProtectedRoutesComponent from "./components/ProtectedRoutesComponent"
 import ListComponent from "./components/management/ListComponent"
 import FormComponent from "./components/management/FormComponent"
 import Error from "./components/common/Error"
@@ -11,33 +11,31 @@ import Error from "./components/common/Error"
 const App = () => {
     const router = createBrowserRouter(
         createRoutesFromElements(
-            <>
-                <Route path="/login" element={ <UserComponent /> } />
+            <Route element={ <Layout /> }>
+                <Route index element={<HomeComponent />}/>
                 
-                <Route element={ <Layout /> }>
-                    <Route index element={<Home />}/>
-                    
-                    <Route element={ <ProtectedRoutesComponent /> }>      
-                        <Route path="/list" element={ <ListComponent /> } />
-                        <Route path="/form" element={ <FormComponent /> } />
-                    </Route>
+                <Route path="login" element={ <LoginComponent /> } />
+                
+                <Route element={ <ProtectedRoutesComponent /> }>      
+                    <Route path="list" element={ <ListComponent /> } />
+                    <Route path="form" element={ <FormComponent /> } />
                 </Route>
 
                 <Route path="*" element={ 
                     <Error 
                         errorTitle="Dirección no encontrada" 
                         errorDescription="La ruta especificada no corresponde a ninguna dirección. Verifique la ruta." 
-                        /> } 
+                    /> }
                 />
-            </>
+            </Route> 
         )
     )
 
     return (
-            <UserProvider>
-                <RouterProvider router={router} />
-            </UserProvider>
-            )
+        <UserProvider>
+            <RouterProvider router={router} />
+        </UserProvider>
+        )
 }
 
 export default App
