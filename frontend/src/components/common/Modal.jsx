ac3197dbed019053dc5d0839f8eec703b4a5fb77
Luke1606 <luisalbertohedzro@gmail.com>
2025-03-06 17:41:11 -0500

    Se hizo mas generico el modal quitandole todo y q solo renderice el children

--- frontend/src/components/common/Modal.jsx
+++ frontend/src/components/common/Modal.jsx
@@ -1,24 +1,19 @@
 import PropTypes from 'prop-types'
 import { createPortal } from 'react-dom'
 
-const Modal = ({ title, children, isOpen }) => {
+const Modal = ({ children, isOpen }) => {
     if (!isOpen) return null
     const parentDiv = document.getElementById("modal")
 
     return createPortal(
         <div className='modal-overlay'>
             <div className='modal-container'>
-                <h1 className='modal-title'>
-                    {title}
-                </h1>
-                
                 {children}
             </div>
         </div>, parentDiv)
 }
 
 Modal.propTypes = {
-    title: PropTypes.string.isRequired,
     children: PropTypes.node.isRequired,
     isOpen: PropTypes.bool.isRequired
 }
