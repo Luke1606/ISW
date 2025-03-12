d70e7669d9780bf66e92ced99378c7129df9b2cd
Luke1606 <luisalbertohedzro@gmail.com>
2025-03-12 16:02:20 -0400

    Se elimino la macro aplicacion management para dividirla en aplicaciones especificas

--- backend/authentication/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from rest_framework_simplejwt.views import TokenObtainPairView
-from rest_framework.permissions import IsAuthenticated
-from . import serializers
-
-
-class LoginTokenObtainPairView(TokenObtainPairView):
-    serializer_class = serializers.LoginTokenObtainPairSerializer
--- backend/ece_api/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class EceApiConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'ece_api'
