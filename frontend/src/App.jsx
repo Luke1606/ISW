76b86085b6392a9cdf9810a3e926f31e90d148b1
Luke1606 <luisalbertohedzro@gmail.com>
2025-03-17 00:28:01 -0400

    Se separo la logica en dos componentes para mayor legibilidad y compatibilidad con futuras versiones

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,42 +1,23 @@
-import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from "react-router-dom"
-import Layout from "./components/common/layout/Layout"
-import HomeComponent from "./components/HomeComponent"
-import LoginComponent from "./components/login/LoginComponent"
+import { Suspense } from "react"
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
+import routes from "./routes"
 import { AuthProvider } from "./contexts/AuthContext"
-import ProtectedRoutesComponent from "./components/ProtectedRoutesComponent"
-import ListComponent from "./components/management/ListComponent"
-import FormComponent from "./components/management/FormComponent"
-import Error from "./components/common/Error"
+
+const router = createBrowserRouter(routes, {
+    future: {
+        v7_startTransition: true,
+        v7_relativeSplatPath: true,
+    },
+})
 
 const App = () => {
-    const router = createBrowserRouter(
-        createRoutesFromElements(
-            <Route element={ <Layout /> }>
-                <Route index element={<HomeComponent />}/>
-                
-                <Route path="login" element={ <LoginComponent /> } />
-                
-                <Route element={ <ProtectedRoutesComponent /> }>      
-                    <Route path="list/:datatype/:index" element={ <ListComponent /> } />
-                  
-                    <Route path="form/:datatype/:index/:view" element={ <FormComponent /> } />
-                  
-                    <Route path="*" element={ 
-                    <Error 
-                        errorTitle="Dirección no encontrada" 
-                        errorDescription="La ruta especificada no corresponde a ninguna dirección. Verifique la ruta." 
-                    /> }
-                />
-                </Route>
-            </Route> 
-        )
-    )
-
     return (
         <AuthProvider>
-            <RouterProvider router={router} />
+            <Suspense fallback={<span className="spin"/>}>
+                <RouterProvider router={router} />
+            </Suspense>
         </AuthProvider>
-        )
+    )
 }
 
 export default App
---
+++ frontend/src/routes.jsx
@@ -0,0 +1,48 @@
+import Layout from "./components/common/layout/Layout"
+import HomeComponent from "./components/HomeComponent"
+import LoginComponent from "./components/login/LoginComponent"
+import ProtectedRoutesComponent from "./components/ProtectedRoutesComponent"
+import ListComponent from "./components/management/ListComponent"
+import FormComponent from "./components/management/FormComponent"
+import Error from "./components/common/Error"
+
+const routes = [
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        index: true,
+        element: <HomeComponent />,
+      },
+      {
+        path: "login",
+        element: <LoginComponent />,
+      },
+      {
+        element: <ProtectedRoutesComponent />,
+        children: [
+          {
+            path: "list/:datatype/:index",
+            element: <ListComponent />,
+          },
+          {
+            path: "form/:datatype/:index/:view",
+            element: <FormComponent />,
+          },
+          {
+            path: "*",
+            element: (
+              <Error
+                errorTitle="Dirección no encontrada"
+                errorDescription="La ruta especificada no corresponde a ninguna dirección. Verifique la ruta."
+              />
+            ),
+          },
+        ],
+      },
+    ],
+  },
+];
+
+export default routes
