0363aa3298f94120cbe4e3f56db923227ce90c0a
Luke1606 <luisalbertohedzro@gmail.com>
2025-03-21 22:15:39 -0400

    Se agrego isProfessor a los permisos

--- backend/backend/utils/permissions.py
+++ backend/backend/utils/permissions.py
@@ -5,22 +5,20 @@ from rest_framework import permissions
 from users.models import Professor
 
 
-class IsDecano(permissions.BasePermission):
-    """
-    Permite acceso solo a los usuarios con el rol de Decano.
-    """
-    def has_permission(self, request, view):
-        # Verifica si el usuario está autenticado y tiene el rol de Decano.
-        return request.user.is_authenticated and getattr(request.user, 'role', None) == 'Decano'
-
-
 class IsStudent(permissions.BasePermission):
     """
     Permite acceso solo a estudiantes.
     """
     def has_permission(self, request, view):
-        # Verificar si el usuario está autenticado y es un estudiante.
-        return request.user.is_authenticated and request.user.is_student()
+        return request.user.is_authenticated and request.user.is_student
+
+
+class IsProfessor(permissions.BasePermission):
+    """
+    Permite acceso solo a profesores.
+    """
+    def has_permission(self, request, view):
+        return request.user.is_authenticated and request.user.is_professor
 
 
 class IsDptoInfProfessor(permissions.BasePermission):
@@ -28,11 +26,15 @@ class IsDptoInfProfessor(permissions.BasePermission):
     Permite acceso solo a profesores del departamento de informática para editar solicitudes.
     """
     def has_permission(self, request, view):
-        return (
-            request.user.is_authenticated
-            and request.user.is_professor()
-            and request.user.professor.role == Professor.Roles.DPTO_INF
-        )
+        return request.user.is_authenticated and request.user.user_role == Professor.Roles.DPTO_INF
+
+
+class IsDecano(permissions.BasePermission):
+    """
+    Permite acceso solo a los usuarios con el rol de Decano.
+    """
+    def has_permission(self, request, view):
+        return request.user.is_authenticated and request.user.user_role == 'Decano'
 
 
 class ReadOnlyForOthers(permissions.BasePermission):
@@ -40,8 +42,4 @@ class ReadOnlyForOthers(permissions.BasePermission):
     Permite acceso de solo lectura a decanos y profesores simples.
     """
     def has_permission(self, request, view):
-        return (
-            request.user.is_authenticated
-            and request.user.is_professor()
-            and request.user.professor.role in {Professor.Roles.PROFESSOR, Professor.Roles.DECANO}
-        )
+        return request.user.is_authenticated and request.user.user_role in {Professor.Roles.PROFESSOR, Professor.Roles.DECANO}
