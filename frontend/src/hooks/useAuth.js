5b3174612e6e8529cee137ca4fe491e9d5a9254b
Luke1606 <luisalbertohedzro@gmail.com>
2025-03-22 22:42:17 -0400

    se integro con el status d sesion de AuthContext para estar pendiente a esto, ademas de que se implemento para q cada 30 segundos verifique el estado del token aun si el status no cambia, aunq actualmente no recarga un nuevo token antes d que expire

--- frontend/src/hooks/useAuth.js
+++ frontend/src/hooks/useAuth.js
@@ -1,8 +1,10 @@
-import { useEffect, useState } from "react"
+import { useContext, useEffect, useState } from "react"
 import AuthService from "../services/AuthService"
+import { AuthContext } from "../contexts/AuthContext"
 
 const useAuth = () => {
     const [isAuthorized, setIsAuthorized] = useState(null)
+    const { authStatusChanged } = useContext(AuthContext)
 
     useEffect(() => {
         const checkAuthorized = async () => {
@@ -10,12 +12,17 @@ const useAuth = () => {
                 const result = await AuthService.checkAuth()
                 setIsAuthorized(result)
             } catch (error) {
-                console.error("Error checking authorization:", error)
+                console.error("Error verificando autorización:", error)
                 setIsAuthorized(false)
             }
         }
         checkAuthorized()
-    }, [])
+        
+        const interval = setInterval(async () => {
+            checkAuthorized()
+        }, 30000)
+        return () => clearInterval(interval)
+    }, [authStatusChanged])
 
     return isAuthorized
 }
