d7be9081c6e3fbee1c3645056df873ebb4e740b8
Luke1606 <luisalbertohedzro@gmail.com>
2025-03-22 22:26:32 -0400

    Se arreglo la logica de de los serializers para que devolvieran correctamente la informacion del model al frontend

--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -8,7 +8,6 @@ class TokenPairSerializer(TokenObtainPairSerializer):
     Esta clase se utiliza para obtener un pair de tokens (access y refresh) para el usuario,
     ademas de devolver la informacion necesaria del usuario al frontend.
     """
-
     @classmethod
     def get_token(cls, user):
         token = super().get_token(user)
@@ -21,42 +20,46 @@ class TokenPairSerializer(TokenObtainPairSerializer):
 
         return token
 
-    def create(self, validated_data):
-        pass
-
-    def update(self, instance, validated_data):
-        pass
-
 
 class TokenRefreshSerializer(TokenRefreshSerializer):
     """
     Esta clase se utiliza para obtener un token de refresh para el usuario.
     """
-
     @classmethod
     def get_token(cls, user):
         return super().get_token(user)
 
-    def create(self, validated_data):
-        pass
 
-    def update(self, instance, validated_data):
-        pass
-
-
-class StudentSerializer(serializers.ModelSerializer):
-    username = serializers.CharField(source='user.username')
+class BaseUserSerializer(serializers.ModelSerializer):
+    """
+    Serializer base para compartir características comunes entre StudentSerializer y ProfessorSerializer.
+    """
+    name = serializers.SerializerMethodField()
     pic = serializers.ImageField(source='user.pic', required=False)
 
+    def get_name(self, obj):
+        """
+        Combina first_name y last_name del usuario.
+        """
+        return f"{obj.user.first_name} {obj.user.last_name}"
+
     class Meta:
+        fields = ['name', 'pic']
+
+
+class StudentSerializer(BaseUserSerializer):
+    """
+    Serializer para estudiantes que hereda de BaseUserSerializer.
+    """
+    class Meta(BaseUserSerializer.Meta):
         model = Student
-        fields = ['username', 'pic', 'faculty', 'group']
+        fields = BaseUserSerializer.Meta.fields + ['faculty', 'group']
 
 
-class ProfessorSerializer(serializers.ModelSerializer):
-    username = serializers.CharField(source='user.username')
-    pic = serializers.ImageField(source='user.pic', required=False)
-
-    class Meta:
+class ProfessorSerializer(BaseUserSerializer):
+    """
+    Serializer para profesores que hereda de BaseUserSerializer.
+    """
+    class Meta(BaseUserSerializer.Meta):
         model = Professor
-        fields = ['username', 'pic', 'role']
+        fields = BaseUserSerializer.Meta.fields + ['role']
