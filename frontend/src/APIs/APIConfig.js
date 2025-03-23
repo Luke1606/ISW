23811302b98ab3a92874d5738f262074c863d4c6
Luke1606 <luisalbertohedzro@gmail.com>
2025-03-22 22:34:41 -0400

    Se modificaron los mensajes de error predeterminados

--- frontend/src/APIs/APIConfig.js
+++ frontend/src/APIs/APIConfig.js
@@ -39,23 +39,24 @@ export const setupInterceptors = (instance) => {
 
                                 // Actualiza el encabezado y reintenta la solicitud original
                                 originalRequest.headers.Authorization = `Bearer ${newAccessToken}`
-                                return instance(originalRequest);
+                                return instance(originalRequest)
                             } catch (error) {
                                 authApi.deleteToken(tokens.ACCESS_TOKEN_KEY)
                                 authApi.deleteToken(tokens.REFRESH_TOKEN_KEY)
                                 console.log(error)
-                                // throw new Error("Unauthorized")
                                 return Promise.reject("Sesión expirada. Por favor, inicia sesión nuevamente.")
                             } 
                         } 
                     }
                     break
                 case 402:
-                    throw new Error("Payment Required")
+                    throw new Error(`Payment Required: ${error.response}`)
                 case 403:
-                    throw new Error("Forbidden")
+                    throw new Error(`Forbidden: ${error.response}`)
                 case 404:
-                    throw new Error("Not Found")
+                    throw new Error(`Not Found: ${error.response}`)
+                case 500:
+                    throw new Error(`Internal Server Issue: ${error.response}`)
                 default:
                     return Promise.reject(error)
             }
