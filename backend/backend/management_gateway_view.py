172834a9954a567ca15a237001fcc3ea1fdb7c26
Luke1606 <luisalbertohedzro@gmail.com>
2025-03-22 22:19:20 -0400

    Se reimplemento completamente el gateway siguiendo un diseño mucho mas simple, optimizado, estandarizado y funcional para cada uno de los methods

--- backend/backend/management_gateway_view.py
+++ backend/backend/management_gateway_view.py
@@ -1,5 +1,7 @@
+import traceback
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.exceptions import ValidationError
 from users.views import StudentViewSet, ProfessorViewSet
 from evidences.views import EvidenceViewSet
 from requests.views import RequestViewSet
@@ -16,133 +18,103 @@ class ManagementGatewayView(BaseModelViewSet):
     Incluye lógica de búsqueda avanzada, paginación y caché.
     """
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.datatype = None
-        self.related_user_id = None
+    VIEWSET_MAPPING = {
+        DataTypes.User.student: StudentViewSet,
+        DataTypes.User.professor: ProfessorViewSet,
+        DataTypes.evidence: EvidenceViewSet,
+        DataTypes.request: RequestViewSet,
+        DataTypes.defense_tribunal: DefenseTribunalViewSet,
+        DataTypes.defense_act: DefenseActViewSet,
+    }
+
+    def initialize_attrs(self, request, **kwargs):
+        self.datatype = kwargs.get("datatype")
+        self.related_user_id = request.query_params.get("related_user_id", None)
+
+        if not self.datatype or self.datatype not in self.VIEWSET_MAPPING:
+            raise ValidationError("Invalid or missing datatype")
 
     def list(self, request, *args, **kwargs):
         """
-        Redirige las solicitudes GET (list) según el `datatype`.
+        Maneja las peticiones de la accion `list`.
         """
-        self.datatype = kwargs.get("datatype")
-        self.related_user_id = request.GET.get("related_user_id")
-
-        # Validar el mapeo del ViewSet basado en el datatype
-        viewset = self.get_viewset_for_action('list')
-        if not viewset:
-            return Response({"error": "Invalid datatype"}, status=status.HTTP_400_BAD_REQUEST)
-
-        # Reutilizar la funcionalidad list del ViewSet
-        return viewset(request._request, *args, **kwargs)
+        return self.execute_viewset('list', request, *args, **kwargs)
 
     def retrieve(self, request, *args, **kwargs):
         """
-        Redirige las solicitudes GET con ID (retrieve).
+        Maneja las peticiones de la accion `retrieve`.
         """
-        self.datatype = kwargs.get("datatype")
-        self.related_user_id = request.GET.get("related_user_id")
-
-        viewset = self.get_viewset_for_action('retrieve')
-        if not viewset:
-            return Response({"error": "Invalid datatype"}, status=status.HTTP_400_BAD_REQUEST)
-
-        return viewset(request._request, *args, **kwargs)
+        return self.execute_viewset('retrieve', request, *args, **kwargs)
 
     def create(self, request, *args, **kwargs):
         """
-        Redirige las solicitudes POST.
+        Maneja las peticiones de la accion `create`.
         """
-        self.datatype = kwargs.get("datatype")
-        self.related_user_id = request.GET.get("related_user_id")
-
-        viewset = self.get_viewset_for_action('create')
-        if not viewset:
-            return Response({"error": "Invalid datatype"}, status=status.HTTP_400_BAD_REQUEST)
-
-        return viewset(request._request, *args, **kwargs)
+        return self.execute_viewset('create', request, *args, **kwargs)
 
     def update(self, request, *args, **kwargs):
         """
-        Redirige las solicitudes PUT.
+        Maneja las peticiones de la accion `update`.
         """
-        self.datatype = kwargs.get("datatype")
-        self.related_user_id = request.GET.get("related_user_id")
-
-        viewset = self.get_viewset_for_action('update')
-        if not viewset:
-            return Response({"error": "Invalid datatype"}, status=status.HTTP_400_BAD_REQUEST)
-
-        return viewset(request._request, *args, **kwargs)
+        return self.execute_viewset('update', request, *args, **kwargs)
 
     def destroy(self, request, *args, **kwargs):
         """
-        Redirige las solicitudes DELETE.
+        Maneja las peticiones de la accion `destroy`.
         """
-        self.datatype = kwargs.get("datatype")
-        self.related_user_id = request.GET.get("related_user_id")
+        return self.execute_viewset('destroy', request, *args, **kwargs)
 
-        viewset = self.get_viewset_for_action('destroy')
-        if not viewset:
-            return Response({"error": "Invalid datatype"}, status=status.HTTP_400_BAD_REQUEST)
+    def execute_viewset(self, action, request, *args, **kwargs):
+        """
+        Ejecuta el viewset correspondiente de manera dinámica a partir del `action` y el `datatype`.
+        """
+        try:
+            viewset = self.get_viewset_for_action(action, request, kwargs)
 
-        return viewset(request._request, *args, **kwargs)
+            if isinstance(viewset, Response):  # Si es una respuesta de error
+                return viewset
+            return viewset(request._request, *args, **kwargs)
+
+        except AttributeError as e:
+            # Imprimir el traceback completo en la consola
+            print("Traceback completo:")
+            traceback.print_exc()
+
+            return Response(
+                {"error": f"Action {action} not implemented for {self.datatype} and caused {str(e)}"},
+                status=status.HTTP_501_NOT_IMPLEMENTED
+            )
+
+    def get_viewset_for_action(self, action, request, kwargs):
+        """
+        Devuelve una vista generada dinámicamente para el ViewSet correspondiente
+        basado en el `datatype` y la acción (list, create, etc.).
+        """
+        # Inicializar atributos (datatype y related_user_id)
+        self.initialize_attrs(request, **kwargs)
+
+        # Obtén el ViewSet y la acción correspondiente
+        view_mapping = self.get_viewset_mapping()
+        viewset_class = view_mapping.get(self.datatype)
+
+        if not viewset_class:
+            return Response(
+                {"error": "Invalid datatype"},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+        if not action:
+            return Response(
+                {"error": f"Unsupported action: {action}."},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+        viewset = viewset_class.as_view({request.method.lower(): action})
+
+        return viewset
 
     def get_viewset_mapping(self):
         """
-        Define el mapeo de ViewSets para cada `datatype`.
+        Devuelve el mapeo de ViewSets para cada `datatype`.
         """
-        return {
-            DataTypes.User.student: StudentViewSet,
-            DataTypes.User.professor: ProfessorViewSet,
-            DataTypes.evidence: EvidenceViewSet,
-            DataTypes.request: RequestViewSet,
-            DataTypes.defense_tribunal: DefenseTribunalViewSet,
-            DataTypes.defense_act: DefenseActViewSet,
-        }
-
-    def get_viewset_for_action(self, action):
-        """
-        Devuelve el método específico de un ViewSet según la acción (list, create, etc.).
-        """
-        view_mapping = self.get_viewset_mapping()
-        viewset = view_mapping.get(self.datatype)
-
-        if not viewset:
-            return None
-        return viewset.as_view({
-            'get': 'list',          # Asocia GET con la acción 'list'
-            'post': 'create',       # Asocia POST con la acción 'create'
-            'put': 'update',        # Asocia PUT con la acción 'update'
-            'delete': 'destroy',    # Asocia DELETE con la acción 'destroy'
-        })
-
-    def get_queryset(self):
-        """
-        Reimplementa la lógica del queryset base según el datatype y related_user_id.
-        """
-        model, _ = self.get_model_and_serializer()
-        if not model:
-            return self.queryset.none()
-
-        queryset = model.objects.all()
-        if self.datatype == DataTypes.User.student and self.related_user_id:
-            queryset = queryset.search(related_user_id=self.related_user_id)
-        return queryset
-
-    def get_model_and_serializer(self):
-        """
-        Devuelve el modelo y el serializador basado en el `datatype`.
-        """
-        model_serializer_mapping = {
-            DataTypes.User.student: StudentViewSet.get_model_and_serializer(),
-            DataTypes.User.professor: ProfessorViewSet.get_model_and_serializer(),
-            DataTypes.evidence: EvidenceViewSet.get_model_and_serializer(),
-            DataTypes.request: RequestViewSet.get_model_and_serializer(),
-            DataTypes.defense_tribunal: DefenseTribunalViewSet.get_model_and_serializer(),
-            DataTypes.defense_act: DefenseActViewSet.get_model_and_serializer(),
-        }
-        model_serializer = model_serializer_mapping.get(self.datatype)
-        if not model_serializer:
-            raise ValueError(f"No se ha definido un modelo/serializador para el datatype: {self.datatype}")
-        return model_serializer
+        return self.VIEWSET_MAPPING
