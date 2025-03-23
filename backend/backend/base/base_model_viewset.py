17352e55a46061785fa534a2d6e4afd83b05859a
Luke1606 <luisalbertohedzro@gmail.com>
2025-03-22 22:20:38 -0400

    Se reimplemento el metodo list y la logica de paginate_queryset, ya es perfectamente funcional y devuelve los datos en la estructura esperada

--- backend/backend/base/base_model_viewset.py
+++ backend/backend/base/base_model_viewset.py
@@ -3,6 +3,8 @@ Un ViewSet base que integra funcionalidad de filtrado dinámico basado en `SEARC
 clase `DynamicFilterSetFactory`.
 """
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.response import Response
+from rest_framework import status
 from django_filters import rest_framework as filters
 from django.core.paginator import Paginator
 from django.core.cache import cache
@@ -32,32 +34,63 @@ class BaseModelViewSet(ModelViewSet):
         factory = DynamicFilterSetFactory(self.queryset.model)
         return factory.create()
 
-    def search_queryset(self, queryset, search_term):
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        return queryset
+
+    def list(self, request, *args, **kwargs):
+        """
+        Sobrescribe el método 'list' para devolver todos los datos, divididos por página.
+        """
+        queryset = self.get_queryset()
+
+        if not queryset.exists():
+            return Response(
+                {"message": "No hay elementos disponibles."},
+                status=status.HTTP_204_NO_CONTENT
+            )
+
+        paginated_data = self.paginate_queryset()
+
+        # Serializar los datos paginados
+        for page, object_list in paginated_data["data"].items():
+            serializer = self.get_serializer(object_list, many=True)
+            paginated_data["data"][page] = serializer.data
+
+        # Devolver la respuesta final
+        print(paginated_data)
+        return Response(paginated_data, status=status.HTTP_200_OK)
+
+    def search_queryset(self, search_term):
         """
         Realiza búsquedas en el queryset utilizando los SEARCHABLE_FIELDS.
         """
         model = self.queryset.model
         searchable_fields = getattr(model, "SEARCHABLE_FIELDS", {})
         if not searchable_fields or not search_term:
-            return queryset.objects.all()
+            return self.queryset.objects.all()
 
         conditions = Q()
         for field, lookup in searchable_fields.items():
             conditions |= Q(**{f"{field}__{lookup}": search_term})
 
-        return queryset.filter(conditions)
+        return self.queryset.filter(conditions)
 
-    def paginate_queryset(self, queryset):
+    def paginate_queryset(self):
         """
-        Maneja la paginación del queryset.
+        Maneja la paginación del queryset y devuelve todos los datos divididos por páginas.
         """
-        page_number = int(self.request.GET.get("page", 1))
-        paginator = Paginator(queryset, self.page_size)
-        page = paginator.get_page(page_number)
+        paginator = Paginator(self.queryset, self.page_size)
+        total_pages = paginator.num_pages
+        all_data = {}
+
+        for page_number in range(0, total_pages):
+            page = paginator.get_page(page_number)
+            all_data[page_number] = page.object_list
+
         return {
-            "object_list": page.object_list,
-            "total_pages": paginator.num_pages,
-            "current_page": page_number,
+            "data": all_data,
+            "total_pages": total_pages
         }
 
     def cache_response(self, cache_key, data, timeout=300):
