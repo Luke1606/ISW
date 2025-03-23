b1a71bf0e6490b78d579de52be45c604231f99f7
Luke1606 <luisalbertohedzro@gmail.com>
2025-03-22 22:36:45 -0400

    Se aplico DRY y se definio una estructura mas estandarizada, simple y legibe para cada metodo

--- frontend/src/APIs/ManagementAPI.js
+++ frontend/src/APIs/ManagementAPI.js
@@ -2,66 +2,31 @@ import { createApiInstance } from "./APIConfig"
 
 const managementApi = createApiInstance("http://localhost:8000/management/")
 
-export const getAllData = async (datatype, relatedUserId) => {
-    if (!datatype) throw new Error('Datatype is required')
-    
+const handleRequest = async (method, url, options = {}) => {
     try {
-        const response = await managementApi.get(`${datatype}/`, { params:{ related_user_id: relatedUserId } })
-        return response.data    
+        const response = await managementApi[method](url, options)
+        return response.data
     } catch (error) {
-        throw new Error("Error en la petición: " + error)
+        const errorStatus = error.request.status
+        const errorHeader = error.request.statusText
+        const errorMessage = error.response.data.error
+        throw new Error(`${errorStatus} ${errorHeader}: ${errorMessage}`)
     }
 }
 
+export const getAllData = (datatype, relatedUserId) =>
+    handleRequest('get', `${datatype}/`, { params: { related_user_id: relatedUserId } })
 
-export const getData = async (datatype, id, relatedUserId) => {
-    if (!datatype || !id) throw new Error('Datatype and id are required')
-    
-    try {
-        const response = await managementApi.get(`${datatype}/`, { params:{ related_user_id: relatedUserId, id: id } })
-        return response.data    
-    } catch (error) {
-        throw new Error("Error en la petición: " + error)
-    }
-    
-}
+export const getData = (datatype, id, relatedUserId) =>
+    handleRequest('get', `${datatype}/${id}/`, { params: { related_user_id: relatedUserId } })
 
+export const createData = (datatype, data, relatedUserId) =>
+    handleRequest('post', `${datatype}/`, { data, params: { related_user_id: relatedUserId } })
 
-export const createData = async (datatype, data, relatedUserId) => {
-    if (!datatype || !data) throw new Error('Datatype and data are required')
-    
-    try {
-        const response = await managementApi.post(`${datatype}/`, { params:{ related_user_id: relatedUserId, data: data } })
-        return response.data    
-    } catch (error) {
-        throw new Error("Error en la petición: ", error)
-    }
-}
+export const updateData = (datatype, id, data, relatedUserId) =>
+    handleRequest('put', `${datatype}/${id}/`, { data, params: { related_user_id: relatedUserId } })
 
-
-export const updateData = async (datatype, id, data, relatedUserId) => {
-    if (!datatype || !id || !data) throw new Error('Datatype, id and new data are required')
-    
-    try {
-        const response = await managementApi.put(`${datatype}/`, { params:{ related_user_id: relatedUserId, id: id, data: data } })
-        return response.data    
-    } catch (error) {
-        throw new Error("Error en la petición: ", error)
-    }
-    
-}
-
-
-export const deleteData = async (datatype, id, relatedUserId) => {
-    if (!datatype || !id) throw new Error('Datatype and id are required')
-    
-    try {
-        const response = await managementApi.delete(`${datatype}/`, { params:{ related_user_id: relatedUserId, id: id } })
-        return response.data    
-    } catch (error) {
-        throw new Error("Error en la petición: ", error)
-    }
-    
-}
+export const deleteData = (datatype, id, relatedUserId) =>
+    handleRequest('delete', `${datatype}/${id}/`, { params: { related_user_id: relatedUserId } })
 
 export default managementApi
