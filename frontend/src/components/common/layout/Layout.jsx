8323e3aeb130c91f157acb1b3f663f47d1d58ac0
Luke1606 <luisalbertohedzro@gmail.com>
2025-03-29 17:26:10 -0400

    Se agrego un boton de Volver el cual no se visualiza en dispositivos moviles con resoluciones menores a la de un tablet

--- frontend/src/components/common/layout/Layout.jsx
+++ frontend/src/components/common/layout/Layout.jsx
@@ -1,13 +1,26 @@
-import { Outlet } from "react-router-dom"
+import { Outlet, useNavigate, useLocation } from "react-router-dom"
+import { ArrowUpLeftSquare } from 'lucide-react';
 import Header from "./Header"
 import Footer from "./Footer"
 
 const Layout = () => {
+    const navigate = useNavigate()
+    const location = useLocation()
+    const currentPath = location.pathname
     return (
-            <div 
+            <div
                 className="layout-container">
                 <Header />
-                    <main className="main-content">
+                    <main className="main-content"
+                        >
+                        { (currentPath != '/' && currentPath != '/login')&&
+                            <button
+                                className="back-button"
+                                onClick={() => navigate(-1)}
+                                title="Volver"
+                                >
+                                <ArrowUpLeftSquare size={40} color="white" />
+                            </button>}
                         <Outlet/>
                     </main> 
                 <Footer />
