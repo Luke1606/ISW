73c02e9cc25a72a5a6d8337e766a76d517bb55bc
Luke1606 <luisalbertohedzro@gmail.com>
2025-04-07 00:29:16 -0400

    Se reimplemento agregando un manejo de errores a la funcion getPrevValues

--- frontend/src/hooks/management/useForm.js
+++ frontend/src/hooks/management/useForm.js
@@ -1,13 +1,29 @@
+import { useEffect, useState } from 'react'
 import useDebouncedApiCall from '../common/useDebouncedApiCall'
 import ManagementService from '../../services/ManagementService'
+import NotificationService from '../../services/NotificationService'
 
 const useForm = (datatype, idData) => {
-    const getPrevValues = useDebouncedApiCall(async (datatype, id) => {
-        const response = await ManagementService.getData(datatype, id)
-        return response? response.data : null
-    })
-    const prevValues = getPrevValues(datatype, idData)
+    const [prevValues, setPrevValues] = useState(null)
 
+    const getPrevValues = useDebouncedApiCall(async (datatype, id) => {
+        try {
+            const response = await ManagementService.getData(datatype, id)
+            setPrevValues(response)
+        } catch (error) {
+            const notification = {
+                title: "Error",
+                message: error.message
+            }
+            NotificationService.showToast(notification, 'error')
+        }
+    })
+
+    useEffect(() => {
+        getPrevValues(datatype, idData)
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [datatype, idData])
+    
     const handleSubmit = idData? 
                             ManagementService.updateData
                             : 
@@ -15,8 +31,6 @@ const useForm = (datatype, idData) => {
 
     return { prevValues, handleSubmit }
 }
-
-
 
 export default useForm
 
