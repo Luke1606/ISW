0595239975acf575ddf956310c8a862ae065a7cd
Luke1606 <luisalbertohedzro@gmail.com>
2025-04-10 00:16:27 -0400

    Se descomento el request form y se tiene pendiente adaptar la logica de dropPopup para que use la logica global de modals

--- frontend/src/components/management/Forms/RequestForms.jsx
+++ frontend/src/components/management/Forms/RequestForms.jsx
@@ -3,22 +3,22 @@
 // import useGenericForm from "../../../hooks/common/useGenericForm"
 // import Modal from "../../common/Modal"
 
-export const RequestForm = (params) => {
-    // return (
-    //     <>
-    //         <label className="form-label" htmlFor="usuario"> Nombre del estudiante: </label>
-    //         <input className="form-input" id="usuario" type="text"  value={prevValues.nombre} />
+export const RequestForm = ({closeModal, prevValues, handleSubmit}) => {
+    return (
+        <>
+            <label className="form-label" htmlFor="usuario"> Nombre del estudiante: </label>
+            <input className="form-input" id="usuario" type="text"  value={prevValues.nombre} readOnly/>
 
-    //         <label className="form-label" htmlFor="cargo-select"> Seleccione el ejercicio deseado: </label>
-    //         <select id="cargo-select">
-    //             <option disabled value=""> -- Escoja una opción -- </option>
-    //             <option value="estudiante">  </option>
-    //             <option value="profesor">  </option>
-    //             <option value="dptoInformatica"> </option>
-    //             <option value="secretaria">  </option>
-    //             <option value="decano">  </option>
-    //         </select>
-    //     </>)
+            <label className="form-label" htmlFor="cargo-select"> Seleccione el ejercicio deseado: </label>
+            <select id="cargo-select">
+                <option disabled value=""> -- Escoja una opción -- </option>
+                <option value="estudiante">  </option>
+                <option value="profesor">  </option>
+                <option value="dptoInformatica"> </option>
+                <option value="secretaria">  </option>
+                <option value="decano">  </option>
+            </select>
+        </>)
 }
 
 export const ReadOnlyRequestForm = (prevValues) => {
