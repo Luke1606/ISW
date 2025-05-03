795eaf6e2640b015797bbaf67788e3278f7c4a7a
Luke1606 <luisalbertohedzro@gmail.com>
2025-05-03 17:58:22 -0400

    Se cambio de comillas dobles a simples

--- backend/core/management/utils/constants.py
+++ backend/core/management/utils/constants.py
@@ -1,24 +1,24 @@
-"""
+'''
 Este modulo contiene los valores constantes utiles en la aplicacion.
-"""
+'''
 
 
 class DataTypes:
-    """
+    '''
     Esta clase dicta los distintos valores y roles para evitar duplicacion.
-    """
+    '''
     class User:
-        """
+        '''
         Esta clase dicta los distintos tipos de usuarios
-        """
-        student = "student"
-        professor = "professor"
-        dptoInf = "dptoInf"
-        decan = "decan"
+        '''
+        student = 'student'
+        professor = 'professor'
+        dptoInf = 'dptoInf'
+        decan = 'decan'
         roles = {student, professor, dptoInf, decan}
 
-    evidence = "evidence"
-    request = "request"
-    defense_tribunal = "defense_tribunal"
-    tribunal = "tribunal"
-    defense_act = "defense_act"
+    evidence = 'evidence'
+    request = 'request'
+    defense_tribunal = 'defense_tribunal'
+    tribunal = 'tribunal'
+    defense_act = 'defense_act'
