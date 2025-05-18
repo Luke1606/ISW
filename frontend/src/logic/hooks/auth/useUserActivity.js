f822e73e633564b6a3a1e41b4fea6958afdf1256
Luke1606 <luisalbertohedzro@gmail.com>
2025-05-18 18:44:48 -0400

    Ya se cierra correctamente la sesion por inactividad

--- frontend/src/logic/hooks/auth/useUserActivity.js
+++ frontend/src/logic/hooks/auth/useUserActivity.js
@@ -1,28 +1,30 @@
 import { useCallback, useEffect, useRef } from 'react'
-import { AuthService, useAuth } from '@/logic'
+import { useAuth } from '@/logic'
 
 /**
- * @description Vigila la actividad del usuario para que al cumplirse {@link timeoutDuration} si el usuario no ha ejecutado ninguno de los eventos en {@link events} se cambie con {@link AuthService.setUserActive} a false y se trate al usuario como inactivo según la configuracion de {@link AuthService}.
+ * @description Vigila la actividad del usuario para que al cumplirse {@link timeoutDuration} si el usuario no ha ejecutado ninguno de los eventos en {@link events} se cierre la sesión con {@link logout}.
  * 
- * @param {number} `timeoutDuration` - Tiempo definido para esperar entre cada comprobacion de inactividad del usuario, por defecto 10 minutos
- * @default 600000
- * @param {Array<string>} `events`- Eventos destinados a monitorizar la actividad del usuario, por defecto son mousemove, keydown y scroll
+ * @param {number} `timeoutDuration` - Tiempo definido para esperar entre cada comprobacion de inactividad del usuario.
+ * @default 600000 (10 minutos)
+ * @param {Array<string>} `events`- Eventos destinados a monitorizar la actividad del usuario.
  * @default ['mousemove','keydown','scroll']
  */
 const useUserActivity = (timeoutDuration = 10 * 60 * 1000, events = ['mousemove', 'keydown', 'scroll']) => {
     if (!events || events.length === 0) events = ['mousemove', 'keydown', 'scroll']
     
     const timeoutRef = useRef(null)
-    const { isAuthorized } = useAuth()
+    const { logout, isAuthorized } = useAuth()
 
     const resetActivityTimeout = useCallback(() => {
         clearTimeout(timeoutRef.current) // Limpia el timeout anterior
 
-        timeoutRef.current = setTimeout(() =>  {
-                if (isAuthorized)
-                    AuthService.closeSession()
+        timeoutRef.current = setTimeout(async () =>  {
+                if (isAuthorized) {
+                    console.warn('Se cerró la sesión por inactividad')
+                    await logout()
+                }
             }, timeoutDuration)
-    }, [isAuthorized, timeoutDuration])
+    }, [isAuthorized, logout, timeoutDuration])
 
     useEffect(() => {
         if (isAuthorized) {
